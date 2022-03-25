5f3ea15775f52fb18f84a6ae796582ee945fe95c
Md. Reyanus Salehin <reyanussalehin@iut-dhaka.edu>
2022-03-25 22:21:20 +0600

    insignificant changes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Question Service has successfully been implemented in Heroku')
+    res.send('Question Service has successfully been deployed in Heroku')
 });
 
 module.exports = router;
